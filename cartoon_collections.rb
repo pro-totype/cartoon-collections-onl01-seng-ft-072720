fe1fe93c3888332af5b09ae790454d6477aab50e
Sean B <butts.sean01@gmail.com>
2020-08-02 22:13:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map{|calls|
-  puts "#{calls}"}
+  planeteer_calls.map.capitalize{|calls|
+  puts "#{calls}!"}
   
 end
 
