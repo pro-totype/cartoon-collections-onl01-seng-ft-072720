70a2148bcf77ff0b32bd5c9c905683f4fa7c3b8d
Sean B <butts.sean01@gmail.com>
2020-08-02 10:03:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+install 
+
+dwarves= ["Doc","Dopey","Bashful","Grumpy"]
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index{|dwarf,index| i= index + 1
+  puts "#{i}.#{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map{|calls| calls= calls.join("!") 
+  puts "#{calls}.capitalize"}
+  
 end
 
 def long_planeteer_calls# code an argument here
