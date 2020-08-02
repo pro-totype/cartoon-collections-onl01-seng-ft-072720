22b113ec176db94eef4d2c83e3294a726f43d85f
Sean B <butts.sean01@gmail.com>
2020-08-02 23:36:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,10 +12,11 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_long)
- i = 3
- call = calls_long.collect
- calling = call.count()
+  i = 3
+  call = calls_long.collect
+  calling = call.count()
   calling >= i ? true : false   
+    yield calling.any
 
 end
 
