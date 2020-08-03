b259f4f03f303e374a732cf4556f4beb4cce16b6
Sean B <butts.sean01@gmail.com>
2020-08-03 00:31:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,6 @@ def long_planeteer_calls(calls_long)
 end
 
 def find_the_cheese# code an argument here
-  # the array below is here to help
+ 
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
