a0e78d8261ecbbf7929f60535d14d2c9cf5e2997
Sean B <butts.sean01@gmail.com>
2020-08-03 00:30:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_long)
-    planeteer_calls.any? { |call| call.length > 4 }
+    calls_long.any? { |call| call.length > 4 }
 end
 
 def find_the_cheese# code an argument here
