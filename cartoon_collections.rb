fe5760b4c45c72d87026a14e546feb06319c9108
Sean B <butts.sean01@gmail.com>
2020-08-03 00:34:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,3 +15,10 @@ def long_planeteer_calls(calls_long)
     calls_long.any? { |call| call.length > 4 }
 end
 
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w[gouda cheddar camembert]
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
+end
